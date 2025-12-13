777e5958de2b852eff632add5b6c4f23c5ebba49
patrickab <patrick.a.bitzer@gmail.com>
2025-12-13 11:03:25 +0100

    increase default GPU utilization

--- src/llm_baseclient/config.py
+++ src/llm_baseclient/config.py
@@ -2,4 +2,4 @@ OLLAMA_PORT = 11434
 
 VLLM_PORT = 8000
 VLLM_BASE_URL = f"http://localhost:{VLLM_PORT}"
-VLLM_GPU_UTIL = 0.7 # Limit vLLM to 0,x% VRAM so local machine doesn't freeze
+VLLM_GPU_UTIL = 0.8 # Limit vLLM to 0,x% VRAM so local machine doesn't freeze
